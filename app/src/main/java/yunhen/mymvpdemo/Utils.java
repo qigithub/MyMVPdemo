12940f22b1a27b84a1b389fcdc3672cd5569ad89
dongqi <997989682@qq.com>
2016-08-23 09:48:10 +0800

    fix

--- app/src/main/java/yunhen/mymvpdemo/Utils.java
+++ app/src/main/java/yunhen/mymvpdemo/Utils.java
@@ -73,7 +73,6 @@ public class Utils {
     }
 
     public static int px2dp(Context context, float pxValue) {
-
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
